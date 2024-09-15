676e92a1bb01bf30120466302ce9107087a403e4
arielrimon <arielrim21@gmail.com>
2024-09-15 16:38:50 +0300

    init

--- service2/main.py
+++ service2/main.py
@@ -1,13 +1,8 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    # Use the PORT environment variable to listen on the correct port
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+def service2_function(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        A simple "Hello, World!" response.
+    """
+    return "Hello, World!"

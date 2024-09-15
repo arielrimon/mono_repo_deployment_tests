b6b379dbd526f26e3c127f591ba66cfc1c93fca0
arielrimon <arielrim21@gmail.com>
2024-09-15 15:46:54 +0300

    init

--- service1/main.py
+++ service1/main.py
@@ -1,2 +1,8 @@
-def hello_world():
+def hello_world(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        A simple "Hello, World!" response.
+    """
     return "Hello, World!"
